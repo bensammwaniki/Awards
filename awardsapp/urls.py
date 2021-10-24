82daa4b819110520c772ea6c1e662adede0b9ef6
bensammwaniki <bensammwaniki@gmail.com>
2021-10-25 01:36:50 +0300

    added profile model

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("", views.home),
+    path("profile/", views.profile),
+
 ]
--- awardsapp/views.py
+++ awardsapp/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
+
+
+@login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    projects = Post.objects.filter(user_id=current_user.id)
+    profile = Profile.objects.filter(user_id=current_user.id).first()
+    return render(request, 'profile.html', {"projects": projects, "profile": profile})
