72aac546230a23fe711c3429b3749219e49d424b
bensammwaniki <bensammwaniki@gmail.com>
2021-10-25 02:35:58 +0300

    added url links fields

--- awardsapp/models.py
+++ awardsapp/models.py
@@ -14,6 +14,7 @@ class Post(models.Model):
     screenshort = CloudinaryField('image')
     project_name = models.CharField(max_length=50)
     project_desc = models.TextField()
+    project_url = models.TextField()
     posted_date = models.DateTimeField(auto_now_add=True)
     profile = models.ForeignKey(User, on_delete=models.CASCADE)
     rate = models.IntegerField(default=0)
